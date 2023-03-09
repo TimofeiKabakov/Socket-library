52f00382031fb52a961b9e19deae7b4e7731e076
Matthew Munro (mam552) <mam552@usask.ca>
2023-03-09 13:15:16 -0600

    Add clang-format.

--- example.c
+++ example.c
@@ -1,6 +1,6 @@
 #include "lib/net.h"
 
 int main() {
-    conn_opt options = {0};
-    tcp_connection *conn = create_tcp_connection(options);
+  conn_opt options = {0};
+  tcp_connection *conn = create_tcp_connection(options);
 }
--- lib/net_windows.c
+++ lib/net_windows.c
@@ -1,62 +1,64 @@
 #include "net.h"
 
 typedef struct tcp_connection {
-    // TODO
+  // TODO
 } tcp_connection;
 
 typedef struct udp_connection {
-    // TODO
+  // TODO
 } udp_connection;
 
 typedef struct remote_ip {
-    // TODO
+  // TODO
 } remote_ip;
 
-
 int Initialize() {
-    // TODO
+  // TODO
 }
 
 remote_ips create_ip_struct(char **ips) {
-    // TODO
+  // TODO
 }
 
 tcp_connection *create_tcp_connection(conn_opt opt) {
-    // TODO
+  // TODO
 }
 
 tcp_connection *destroy_tcp_connection() {
-    // TODO
+  // TODO
 }
 
-int send_tcp_message(tcp_connection *conn, remote_ips remotes, void *data, size_t len) {
-    // TODO
+int send_tcp_message(tcp_connection *conn, remote_ips remotes, void *data,
+                     size_t len) {
+  // TODO
 }
 
 int receive_tcp_message_async(tcp_connection *conn, void **data, size_t *len) {
-    // TODO
+  // TODO
 }
 
 int receive_tcp_message(tcp_connection *conn, void **data, size_t *len) {
-    // TODO
+  // TODO
 }
 
 udp_connection *create_udp_connection(conn_opt opt) {
-    // TODO
+  // TODO
 }
 
 udp_connection *destroy_udp_connection() {
-    // TODO
+  // TODO
 }
 
-int send_udp_message(udp_connection *conn, remote_ips remotes, void *data, size_t len) {
-    // TODO
+int send_udp_message(udp_connection *conn, remote_ips remotes, void *data,
+                     size_t len) {
+  // TODO
 }
 
-remote_ip *receive_udp_message_async(udp_connection *conn, void **data, size_t *len) {
-    // TODO
+remote_ip *receive_udp_message_async(udp_connection *conn, void **data,
+                                     size_t *len) {
+  // TODO
 }
 
 remote_ip *receive_udp_message(udp_connection *conn, void **data, size_t *len) {
-    // TODO
+  // TODO
 }
