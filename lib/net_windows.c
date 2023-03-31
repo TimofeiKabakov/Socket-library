701268e6f3566673f173729b4080ac9658fbb24b
Xianglong Du <xianglong.du@usask.ca>
2023-03-31 17:57:06 -0600

    updated Makefile; finished some chores related to code style

--- lib/net_windows.c
+++ lib/net_windows.c
@@ -6,44 +6,31 @@
 
 #include "net.h"
 
-// #pragma comment(lib, "ws2_32.lib")
-
-// TODO: move to net.h
-#define RECV_BUF_SIZE 64
-#define PORT_STRLEN 6
+/* #pragma comment(lib, "ws2_32.lib") */
 
 typedef struct tcp_connection {
   int listenSockFD;
+
   int *incomingFDs;
   size_t numIncomingFDs;
   size_t maxIncomingFDs;
+
   int *outgoingFDs; 
   size_t numOutgoingFDs;
   size_t maxOutgoingFDs;
+
   conn_opt options;
   int uid;
   int protocolVerPlatSpecific;
-  SRWLOCK mutex;
-} tcp_connection;
 
-/* contains information about the Windows Sockets implementation */
-WSADATA wsaData; 
+  SRWLOCK mutex;
+
+  WSADATA wsaData;
+} tcp_connection;
 
 tcp_connection activeConnections[MAX_CONNECTION_OBJECTS];
 size_t connObjects = 0;
 int nextUID = 0;
-
-typedef struct udp_connection {
-  // TODO
-} udp_connection;
-
-// typedef struct remote_ip {
-//   int protocolVer;
-//   union {
-//     struct sockaddr_in ipv4;
-//     struct sockaddr_in6 ipv6;
-//   } ipData;
-// } remote_ip;
 
 typedef struct remote_ip_handle {
   int protocolVer;
@@ -55,17 +42,11 @@ typedef struct remote_ip_handle {
 } remote_ip_handle;
 
 void Initialize() {
-  // WSAStartup initiates use of WS2_32.dll
-  int rc = WSAStartup(MAKEWORD(2,2), &wsaData);
-  if (rc != 0) {
-    printf("WSAStartup failed: %d\n", rc);
-  } else {
-    for (int i = 0; i < MAX_CONNECTION_OBJECTS; i++) {
-      activeConnections[i].uid = -1;
-      activeConnections[i].listenSockFD = -1;
-      activeConnections[i].numIncomingFDs = 0;
-      activeConnections[i].numOutgoingFDs = 0;
-    }
+  for (int i = 0; i < MAX_CONNECTION_OBJECTS; i++) {
+    activeConnections[i].uid = -1;
+    activeConnections[i].listenSockFD = -1;
+    activeConnections[i].numIncomingFDs = 0;
+    activeConnections[i].numOutgoingFDs = 0;
   }
 }
 
@@ -95,32 +76,6 @@ remote_ips process_tcp_sock_addresses(tcp_connection *conn, const char **ips, co
       break;
     }
 
-    // if ((rc = getaddrinfo(ips[i], ports[i], &hints, &res)) == 0) {
-    //   struct addrinfo *candidate = NULL;
-    //   for (candidate = res; candidate != NULL; candidate = candidate->ai_next) {
-    //     remote_ip ip = {0};
-    //     ip.handle = calloc(1, sizeof(remote_ip_handle));
-
-    //     if (candidate->ai_family == AF_INET) {
-    //       ip.handle->protocolVer = IPV4;
-    //       memcpy(&ip.handle->ipData.ipv4, (struct sockaddr_in *)candidate->ai_addr, sizeof(struct sockaddr_in));
-    //       ip.addr = malloc(INET_ADDRSTRLEN + 1);
-    //     } else {
-    //       ip.handle->protocolVer = IPV6;
-    //       memcpy(&ip.handle->ipData.ipv6, (struct sockaddr_in6 *)candidate->ai_addr, sizeof(struct sockaddr_in6));
-    //       ip.addr = malloc(INET6_ADDRSTRLEN + 1);
-    //     }
-
-    //     ip.port = malloc(PORT_STRLEN);
-    //     strcpy(ip.port, ports[i]);
-    //     ipList.ips[ipList.len] = ip;
-    //     ipList.len++;
-    //     break;
-    //   }
-    //   freeaddrinfo(res);
-    //   WSACleanup();
-    // }
-
     rc = getaddrinfo(ips[i], ports[i], &hints, &res);
     if (rc == 0) {
       remote_ip ip = {0};
@@ -142,7 +97,6 @@ remote_ips process_tcp_sock_addresses(tcp_connection *conn, const char **ips, co
       ipList.len++;
 
       freeaddrinfo(res);
-      // WSACleanup();
     }
   }
   return ipList;
@@ -161,6 +115,9 @@ tcp_connection *create_tcp_connection(conn_opt opt) {
       activeConnections[i].maxIncomingFDs = 10;
       activeConnections[i].maxOutgoingFDs = 10;
       activeConnections[i].protocolVerPlatSpecific = activeConnections[i].options.ver == IPV4 ? AF_INET : AF_INET6 ;
+
+      /* WSAStartup() initiates use of WS2_32.dll */
+      WSAStartup(MAKEWORD(2,2), &activeConnections[i].wsaData);
 
       /* initiate socket file descriptor */
       int iCandidate, iResult;
@@ -245,6 +202,7 @@ int destroy_tcp_connection(tcp_connection *conn) {
   free(conn->outgoingFDs);
   memset(conn, 0, sizeof(tcp_connection));
   conn->uid = -1;  // Mark this slot as free
+  WSACleanup();
   return 0;
 }
 
@@ -254,7 +212,6 @@ int tcp_listen(tcp_connection *conn) {
   if (listenReturn == SOCKET_ERROR) {
     printf("Listen failed with error: %d\n", WSAGetLastError() );
     closesocket(conn->listenSockFD);
-    WSACleanup();
     return 1;
   }
 
@@ -350,7 +307,7 @@ int send_tcp_message(tcp_connection *conn, remote_ips remotes, void *data,
 
 int receive_tcp_message_async(tcp_connection *conn, remote_ips ips, int senderIdx, void **data, size_t *len) {
   // TODO: test needed; is user expected to free *data on failure?
-  *data = malloc(RECV_BUF_SIZE);
+  *data = malloc(RECV_BUFLEN);
   if (senderIdx > ips.len -1) {
     return -1;
   }
@@ -367,7 +324,7 @@ int receive_tcp_message_async(tcp_connection *conn, remote_ips ips, int senderId
 
   int nBytesRecved, res = select(sender->handle->fd + 1, &singleset, NULL, NULL, &timeval);
   if (res > 0 && FD_ISSET(sender->handle->fd, &singleset)) {
-    nBytesRecved = recv(sender->handle->fd, *data, RECV_BUF_SIZE, 0);
+    nBytesRecved = recv(sender->handle->fd, *data, RECV_BUFLEN, 0);
     
     /* TODO: 0 or SOCKET_ERROR */
     if (nBytesRecved == 0) {
@@ -396,14 +353,14 @@ int receive_tcp_message_async(tcp_connection *conn, remote_ips ips, int senderId
 
 int receive_tcp_message(tcp_connection *conn, remote_ips ips, int senderIdx, void **data, size_t *len) {
   // TODO: test needed
-  *data = malloc(RECV_BUF_SIZE);
+  *data = malloc(RECV_BUFLEN);
   if (senderIdx > ips.len - 1) {
     return -1;
   }
 
   remote_ip *sender = &ips.ips[senderIdx];
 
-  int nBytesRecved = recv(sender->handle->fd, *data, RECV_BUF_SIZE, 0);
+  int nBytesRecved = recv(sender->handle->fd, *data, RECV_BUFLEN, 0);
   if (nBytesRecved == 0) {
     AcquireSRWLockExclusive(&conn->mutex);
     for (int j = 0; j < conn->numOutgoingFDs; j++) {
@@ -500,9 +457,9 @@ remote_ip *accept_remote_connection(tcp_connection *conn) {
     newFD = accept(conn->listenSockFD, (struct sockaddr *) &ip->handle->ipData.ipv4, &addrlen);
 
     if (newFD == INVALID_SOCKET) {
-        printf("accept failed: %d\n", WSAGetLastError());
-        closesocket(newFD);
-        return NULL;
+      printf("accept failed: %d\n", WSAGetLastError());
+      closesocket(newFD);
+      return NULL;
     }
 
     ip->addr = malloc(INET_ADDRSTRLEN + 1);
@@ -511,13 +468,12 @@ remote_ip *accept_remote_connection(tcp_connection *conn) {
     sprintf(ip->port, "%u", ip->handle->ipData.ipv4.sin_port);
   } else {
     socklen_t addrlen = sizeof(ip->handle->ipData.ipv6);
-    newFD = accept(conn->listenSockFD, (struct sockaddr *)&ip->handle->ipData.ipv6,
-                   &addrlen);
+    newFD = accept(conn->listenSockFD, (struct sockaddr *)&ip->handle->ipData.ipv6, &addrlen);
 
     if (newFD == INVALID_SOCKET) {
-        printf("accept failed: %d\n", WSAGetLastError());
-        closesocket(newFD);
-        return NULL;
+      printf("accept failed: %d\n", WSAGetLastError());
+      closesocket(newFD);
+      return NULL;
     }
 
     ip->addr = malloc(INET6_ADDRSTRLEN + 1);
